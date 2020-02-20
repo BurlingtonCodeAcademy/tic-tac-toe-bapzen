986ac3879af13011c3d49885f14f5f997d7ecd5c
bapzen <ben@thebencurve.com>
2020-02-20 13:19:31 -0500

    Initial branch commit, a few ideas

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 let startBtn = document.getElementById('start');
-let turnMsg = document.getElementById('turn');
+let turn = document.getElementById('turn');
+let turnX = document.getElementById('x-turn');
+let turnO = document.getElementById('o-turn');
 
 function updateStatus () {
-    turnMsg.removeAttribute("hidden");
+    
+    turnX.setAttribute("hidden");
     startBtn.setAttribute("disabled", "");
 
 }
 
+function takeTurn () {
+    
+}
+
 startBtn.addEventListener('click', updateStatus);
+
+board.addEventListener('click', );
