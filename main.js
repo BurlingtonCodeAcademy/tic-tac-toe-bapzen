74e7850ef5d1844bea06ec718a93988110a1718b
bapzen <ben@thebencurve.com>
2020-02-20 14:35:59 -0500

    Attempt to write play logic

--- main.js
+++ main.js
@@ -2,18 +2,40 @@ let startBtn = document.getElementById('start');
 let turn = document.getElementById('turn');
 let turnX = document.getElementById('x-turn');
 let turnO = document.getElementById('o-turn');
+let board = document.getElementsByClassName('board');
+
+
+let roles = ['x', 'o'];
+let whoNow = roles[0];
 
 function updateStatus () {
-    
-    turnX.setAttribute("hidden");
+    whoNow = roles[0];
+    turnX.removeAttribute("hidden", "");
     startBtn.setAttribute("disabled", "");
 
 }
 
-function takeTurn () {
+function move (target) {
+    event.target.textContent = whoNow;
+
+    if (whoNow === 'x') {
+        whoNow = roles[1];
+    } else if (whoNow === 'o') {
+        whoNow = roles[0];
+    };
     
 }
 
+function doTurn () {
+    for (let cell of board) {
+        cell = board.getElementById('cell')
+        cell.addEventListener('click', move)
+    }
+
+
+}
+
+
 startBtn.addEventListener('click', updateStatus);
 
-board.addEventListener('click', );
+// board.addEventListener('click', );
