6b7723329f2c7b7a00be9e29fba63482d7a36a3d
bapzen <ben@thebencurve.com>
2020-02-21 11:14:30 -0500

    Add click on cell to place player X's mark using nested for..of iterator to place event listeners on each cell

--- main.js
+++ main.js
@@ -2,40 +2,24 @@ let startBtn = document.getElementById('start');
 let turn = document.getElementById('turn');
 let turnX = document.getElementById('x-turn');
 let turnO = document.getElementById('o-turn');
-let board = document.getElementsByClassName('board');
-
+let board = document.querySelectorAll('.board');
+// let rows = board.childNodes;
 
 let roles = ['x', 'o'];
 let whoNow = roles[0];
 
-function updateStatus () {
+function updateStatus() {
     whoNow = roles[0];
     turnX.removeAttribute("hidden", "");
     startBtn.setAttribute("disabled", "");
 
 }
 
-function move (target) {
-    event.target.textContent = whoNow;
-
-    if (whoNow === 'x') {
-        whoNow = roles[1];
-    } else if (whoNow === 'o') {
-        whoNow = roles[0];
-    };
-    
-}
-
-function doTurn () {
-    for (let cell of board) {
-        cell = board.getElementById('cell')
-        cell.addEventListener('click', move)
-    }
-
-
-}
-
-
 startBtn.addEventListener('click', updateStatus);
 
-// board.addEventListener('click', );
+for (let row of board) {
+    let cells = row.childNodes;
+    for (let cell of cells) {
+        cell.addEventListener('click', (event) => { event.target.textContent = whoNow; });
+    };
+}
