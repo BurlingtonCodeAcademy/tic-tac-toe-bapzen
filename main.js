15cfeb611e8fa7703059283e7d1b79d29cc4628b
bapzen <ben@thebencurve.com>
2020-02-21 15:57:49 -0500

    Complete 'No Rules' story with alternating placement of symbols and player turn changing

--- main.js
+++ main.js
@@ -3,23 +3,43 @@ let turn = document.getElementById('turn');
 let turnX = document.getElementById('x-turn');
 let turnO = document.getElementById('o-turn');
 let board = document.querySelectorAll('.board');
-// let rows = board.childNodes;
 
-let roles = ['x', 'o'];
-let whoNow = roles[0];
+const roles = ['x', 'o'];
+let whoNow = 'x';
+turnX.setAttribute("hidden", "");
+turnO.setAttribute("hidden", "");
 
 function updateStatus() {
-    whoNow = roles[0];
     turnX.removeAttribute("hidden", "");
     startBtn.setAttribute("disabled", "");
 
 }
 
-startBtn.addEventListener('click', updateStatus);
+function gamePlay() {
+    for (let row of board) {
+        let cells = row.childNodes;
+        for (let cell of cells) {
+            cell.addEventListener('click', (event) => { event.target.textContent = whoNow; changeTurn(); });
+            
+        };
+    
+    }
+  
+}
 
-for (let row of board) {
-    let cells = row.childNodes;
-    for (let cell of cells) {
-        cell.addEventListener('click', (event) => { event.target.textContent = whoNow; });
+function changeTurn () {
+    if (whoNow === 'x') {
+        turnX.setAttribute("hidden", "");
+        turnO.removeAttribute("hidden");
+        whoNow = 'o';
+    } else if (whoNow === 'o') {
+        turnO.setAttribute("hidden", "");
+        turnX.removeAttribute("hidden");
+        whoNow = 'x';
     };
 }
+
+startBtn.addEventListener('click', updateStatus);
+gamePlay();
+
+
